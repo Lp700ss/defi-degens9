4fa0f65082bea8512a4761b7bc574025a71f8e44
Siddhant Saikia <47236119+Lp700ss@users.noreply.github.com>
2022-06-07 12:35:28 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -32,10 +32,10 @@ const Navbar = () => {
         <div className="gpt3__navbar-menu_container scale-up-center">
           <div className="gpt3__navbar-menu_container-links">
             <p><a href="#home">Home</a></p>
-            <p><a href="#wgpt3">What is GPT3?</a></p>
-            <p><a href="#possibility">Open AI</a></p>
-            <p><a href="#features">Case Studies</a></p>
-            <p><a href="#blog">Library</a></p>
+            <p><a href="#wgpt3">About</a></p>
+            <p><a href="#possibility">What&apos;s Next</a></p>
+            <p><a href="#features">Services</a></p>
+            <p><a href="#blog">Gallery</a></p>
           </div>
           <div className="gpt3__navbar-menu_container-links-sign">
             <p>Sign in</p>
